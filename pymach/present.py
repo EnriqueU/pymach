5d4b9baa9cb4955e9c900f96a225766ecc8eae44
EnriqueU <epeinador@uni.pe>
2018-09-05 18:38:27 -0500

    DoingLogin

--- pymach/present.py
+++ pymach/present.py
@@ -31,8 +31,8 @@ app.config['MODELS_DIR'] = os.path.join(APP_PATH, 'models')
 app.config['MARKET_DIR'] = os.path.join(APP_PATH, 'market')
 ALLOWED_EXTENSIONS = ['txt', 'csv', 'ml', 'html']
 
-#login_manager = LoginManager()
-#login_manager.init_app(app)
+login_manager = LoginManager()
+login_manager.init_app(app)
 #googlelogin = GoogleLogin(app, login_manager)
 
 
@@ -146,7 +146,30 @@ def allowed_file(file_name):
 def dashboard():
     return render_template("home.html")
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    # Here we use a class of some kind to represent and validate our
+    # client-side form data. For example, WTForms is a library that will
+    # handle this for us, and we use a custom LoginForm to validate.
+    form = LoginForm()
+    if form.validate_on_submit():
+        # Login and validate the user.
+        # user should be an instance of your `User` class
+        login_user(user)
+
+        flask.flash('Logged in successfully.')
+
+        next = flask.request.args.get('next')
+        # is_safe_url should check if the url is safe for redirects.
+        # See http://flask.pocoo.org/snippets/62/ for an example.
+        if not is_safe_url(next):
+            return flask.abort(400)
+
+        return flask.redirect(next or flask.url_for('index'))
+    return flask.render_template('login.html', form=form)
+
 @app.route('/defineData', methods=['GET', 'POST'])
+@login_required
 def defineData():
     """  Show the files that have been uploaded """
     dirs = os.listdir(app.config['UPLOAD_DIR'])
